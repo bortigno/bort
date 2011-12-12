dead859ff2f758427e7bc1f915ac4d2b22a3dcb3
bortigno <>
2011-12-12 18:28:44 +0000

    *** empty log message ***

--- VHbbNtupleViewer/make_histos_step2.cxx
+++ VHbbNtupleViewer/make_histos_step2.cxx
@@ -5,7 +5,7 @@
 #include "CutsAndHistos.h"
 #include "Histos.h"
 //#include "Cuts200X.h"
-#include "CutsLP.h"
+#include "CutsNov11.h"
 #include "ntupleReader.h"
 
 void prepareAllZHistos(std::vector<CutsAndHistos *> & allHistosZ,TFile *fout  )
@@ -20,7 +20,7 @@ void prepareAllZHistos(std::vector<CutsAndHistos *> & allHistosZ,TFile *fout  )
   //  allHistosZ.push_back(new CutsAndHistos(new TTbarRegionHZee,new MVAHistos(Zee110)));
   allHistosZ.push_back(new CutsAndHistos(new SignalRegionHZee,new StandardHistos));
   //  allHistosZ.push_back(new CutsAndHistos(new SignalRegionHZee,new MVAHistos(Zee110)));
-  allHistosZ.push_back(new CutsAndHistos(new BDTRegionHZee,new MVAHistos(Zee115)));
+  //  allHistosZ.push_back(new CutsAndHistos(new BDTRegionHZee,new MVAHistos(Zee115)));
   for(size_t a=0;a < allHistosZ.size(); a++)
     {
       allHistosZ[a]->book(*fout);
@@ -33,7 +33,8 @@ int main(int argc, char **argv)
   std::cout << "Hello word" << std::endl;
 
   bool verbose_ = true;
-
+  double fa = 0.46502;
+  double fb = 0.53498;
   Double_t eventWeight=0;
   int event_all=0;
   int event_all_b=0;
@@ -49,7 +50,7 @@ int main(int argc, char **argv)
 
   if(argc < 4)
     {
-      std::cout << "Usage:\n   make_histos inputFileList outputPrefix [mc|mcSpli|data]" << std::endl;
+      std::cout << "Usage:\n   make_histos_step2 inputFile outputPrefix [mc|mcSpli|data]" << std::endl;
       exit(1);
     }
 
@@ -67,11 +68,12 @@ int main(int argc, char **argv)
   //TFile f("../test/PAT.edm.root");
   //  TFile f("../test/VHbbPAT.edm_6_0_HAB.root");
   //ddTFile &f =* TFile::Open("/scratch/arizzi/VHbbPAT.edm_9_0_tPZ.root");
-  TFile *fout = new TFile((name+"_histos.root").c_str(),"RECREATE");
+  TFile *fout = new TFile((name+".histos.root").c_str(),"RECREATE");
   if(verbose_)
     std::cout << "Creating the histograms" << std::endl;
   TH1F * countAll = new TH1F("Count","Count",1,0,2);
   TH1F * countAllWithPU =  new TH1F("CountWithPU","CountWithPU",1,0,2);
+  TH1F * countAllWithPU2011B =  new TH1F("CountWithPU2011B","CountWithPU2011B",1,0,2);
 
   if(verbose_)
     std::cout << "Preparing the Z histos" << std::endl;
@@ -85,18 +87,23 @@ int main(int argc, char **argv)
   std::vector<CutsAndHistos *> allHistosLZ;
   TFile *foutB,*foutC,*foutL;
   TH1F *countB, *countBWithPU,*countC, *countCWithPU,*countL, *countLWithPU;
+  TH1F *countBWithPU2011B, *countCWithPU2011B, *countLWithPU2011B;
+
   if(splitBCLIGHT)
     {
       std::cout << "Enabling split" << std::endl;
       foutB = new TFile((name+"_histosB.root").c_str(),"RECREATE");
       countB = new TH1F("Count","Count",1,0,2);
       countBWithPU =  new TH1F("CountWithPU","CountWithPU",1,0,2);
+      countBWithPU2011B =  new TH1F("CountWithPU2011B","CountWithPU2011B",1,0,2);
       foutC = new TFile((name+"_histosC.root").c_str(),"RECREATE");
       countC = new TH1F("Count","Count",1,0,2);
       countCWithPU =  new TH1F("CountWithPU","CountWithPU",1,0,2);
+      countCWithPU2011B =  new TH1F("CountWithPU2011B","CountWithPU2011B",1,0,2);
       foutL = new TFile((name+"_histosL.root").c_str(),"RECREATE");
       countL = new TH1F("Count","Count",1,0,2);
       countLWithPU =  new TH1F("CountWithPU","CountWithPU",1,0,2);
+      countLWithPU2011B =  new TH1F("CountWithPU2011B","CountWithPU2011B",1,0,2);
       prepareAllZHistos(allHistosBZ,foutB);
       prepareAllZHistos(allHistosCZ,foutC);
       prepareAllZHistos(allHistosLZ,foutL);
@@ -114,14 +121,26 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  countAll->Add((TH1F*)f->Get("Count"));
-  countAllWithPU->Add((TH1F*)f->Get("CountWithPU"));
-  countB->Add((TH1F*)f->Get("Count"));
-  countBWithPU->Add((TH1F*)f->Get("CountWithPU"));
-  countC->Add((TH1F*)f->Get("Count"));
-  countCWithPU->Add((TH1F*)f->Get("CountWithPU"));
-  countL->Add((TH1F*)f->Get("Count"));
-  countLWithPU->Add((TH1F*)f->Get("CountWithPU"));
+  std::cout << "File succesfully opened" << std::endl;
+
+  if(!data){
+    if(verbose_)
+      std::cout << "Adding coutn histograms" << std::endl;
+    countAll->Add((TH1F*)f->Get("Count"));
+    countAllWithPU->Add((TH1F*)f->Get("CountWithPU"));
+    countAllWithPU2011B->Add((TH1F*)f->Get("CountWithPU2011B"));
+    countB->Add((TH1F*)f->Get("Count"));
+    countBWithPU->Add((TH1F*)f->Get("CountWithPU"));
+    countBWithPU2011B->Add((TH1F*)f->Get("CountWithPU2011B"));
+    countC->Add((TH1F*)f->Get("Count"));
+    countCWithPU->Add((TH1F*)f->Get("CountWithPU"));
+    countCWithPU2011B->Add((TH1F*)f->Get("CountWithPU2011B"));
+    countL->Add((TH1F*)f->Get("Count"));
+    countLWithPU->Add((TH1F*)f->Get("CountWithPU"));
+    countLWithPU2011B->Add((TH1F*)f->Get("CountWithPU2011B"));
+  }
+  if(verbose_)
+    std::cout << "Count: " << countAll->GetBinContent(1) <<  std::endl;
 
   ntupleReader event(inputFile.c_str());
   bool trigger = true;
@@ -131,7 +150,7 @@ int main(int argc, char **argv)
     event.GetEntry(iEvent);
     event_all++;
     if(data == false)
-      eventWeight = event.PUweight*event.weightTrig;
+      eventWeight = (fa*event.PUweight+fb*event.PUweight2011B)*event.weightTrig;
     else{
       eventWeight = 1;
       trigger = false;
@@ -158,12 +177,12 @@ int main(int argc, char **argv)
 	  allHistosLZ[a]->process(event,eventWeight);
       }
     }
-      
+    
     for(size_t a=0;a < allHistosZ.size(); a++)
       {
 	allHistosZ[a]->process(event,eventWeight);
       }
-      
+    
   }
     
   fout->Write();
@@ -181,6 +200,7 @@ int main(int argc, char **argv)
   }
 
   f->Close();
+
   std::cout << "TOT: " << event_all << " b: " << event_all_b << " c: "<<  event_all_c <<" l: " << event_all_l <<  std::endl;
 
   
--- VHbbNtupleViewer/plottingmacro.C
+++ VHbbNtupleViewer/plottingmacro.C
@@ -1,6 +1,7 @@
-#include <samples.h>
+#include <sampleCollection.h>
 #include <iostream> 
 #include <TCanvas.h>
+#include <TLine.h>
 #include <TRegexp.h>
 #include <TLegend.h>
 #include <THStack.h>
@@ -12,18 +13,24 @@
 void plottingmacro()
 {
 
+
+  double fa = 0.46502;
+  double fb = 0.53498;
   bool debug_ = true;
+
+  //  std::string path("Nov10thFall11Plots/");
+  std::string path("Nov10Fall1160MTopSlimPlots/");
 
   if(debug_)
     std::cout << "Init the style form setTDRStyle" << std::endl;
   setTDRStyle();
   gROOT->ForceStyle();
   initOptions();
-  std::map<std::string,TH1F *> grouped;
 
   if(debug_)
     std::cout << "Init the sample" << std::endl;
-  std::vector<Sample> s = samples();
+  //  std::vector<Sample> s = Nov10thDiJetPtUpdatedSlimHistos();
+  std::vector<Sample> s = Nov10Fall1160MTopSlimHistos();
   Sample data(1,"fake data","S1.root",0,true,1000);
 
   if(debug_)
@@ -36,7 +43,7 @@ void plottingmacro()
 
   if(debug_)
     std::cout << "Init the mc sample" << std::endl;
-  for(size_t i=0;i< s.size();i++) s[i].dump(data.lumi());
+  for(size_t i=0;i< s.size();i++) s[i].dump(1,fa,fb);
 
   std::vector<std::string> names;
 
@@ -67,14 +74,17 @@ void plottingmacro()
 
   for(size_t i = 0 ; i < names.size() ; i++) 
     {
+      
+      std::map<std::string,TH1F *> grouped;
       TString n=names[i];
-      //      if(!n.Contains(TRegexp(".*light.*"))) continue;
+      //if(!n.Contains(TRegexp("VlightRegionHZee/HiggsPtVlightRegionHZee"))) continue;
+      //      if(!n.Contains(TRegexp("VlightRegionHZee"))) continue;
 
       if(debug_)
 	std::cout << "Creating the Canvas" << std::endl;
 
       TCanvas *c = new TCanvas();
-      c->SetLogy(true);
+      c->SetLogy(false);
       c->SetTitle(names[i].c_str());
 
       if(debug_)
@@ -84,42 +94,136 @@ void plottingmacro()
       Options o=options[names[i]];
       hd->Rebin(o.rebin);
       hd->SetMarkerStyle(20);
-      hd->Draw("E1");
+      //      hd->Draw("E1");
       hd->SetYTitle(o.yaxis.c_str());
+      double nbin = hd->GetNbinsX();
+      double min_bin = hd->GetMinimumBin();
+      double max_bin = hd->GetMaximumBin();
+      TH1F *hmc = new TH1F("hmc","hmc", nbin, min_bin, max_bin);
+      hmc->Rebin(o.rebin);
 
       if(debug_)
 	std::cout << "Creating the THStack and Legend" << std::endl;
       THStack * sta = new THStack("sta",hd->GetTitle());
       TLegend * l = new TLegend(o.legendx1,o.legendy1,o.legendx2,o.legendy2); //0.7,0.1,0.9,0.6);
+      l->SetFillColor(kWhite);
+      l->SetBorderSize(0);
       if(debug_)
-	std::cout << "Adding entry to the THStack" << std::endl;  
+	std::cout << "Adding data to the legend" << std::endl;  
       l->AddEntry(hd, "Data","LP");
+      if(debug_)
+	std::cout << "Adding MC to the THStack" << std::endl;  
 
+      //with the proper trigger eff
+      //      double SF[] = {1.01,1.03,1.00};
+      //      double SF[] = {1.03,1.054,1.032};
+      double SF[] = {1.0,1.0,1.0};
+
+      if(debug_){
+	for(int i = 0; i< 3; ++i)
+	  std::cout << "SF [" << i << "] = " << SF[i] << std::endl;
+      }
+
+      double mcIntegral=0;
       for(size_t j=0;j< s.size() ;j++) 
 	{ 
 	  if(!s[j].data) 
 	    {
+	      if(debug_)
+		std::cout << "Creating TH1F from file " << s[j].name << std::endl;  
 	      TH1F * h = ((TH1F*)s[j].file()->Get(names[i].c_str()));
-	      h->Scale(s[j].scale(data.lumi()));
+	      if(debug_)
+		std::cout << "Created TH1F from file " << s[j].name << std::endl;  
+	      if(debug_){
+		std::cout << "Scaling : " << s[j].scale(data.lumi(),fa,fb) << std::endl;  
+		std::cout << "Scaling with SF : " << s[j].scale(data.lumi(),fa,fb,SF) << std::endl;  
+		std::cout << "Histo integral before scaling = " << h->Integral() << std::endl;
+	      }
+	      h->Scale(s[j].scale(data.lumi(),fa,fb,SF));
+	      if(debug_){
+		std::cout << "Managing style... " << std::endl;  
+		std::cout << "Histo integral after scaling = " << h->Integral() << std::endl;
+	      }
 	      h->SetLineWidth(1.);
 	      h->SetFillColor(s[j].color);
+	      h->SetLineColor(s[j].color);
 	      h->Rebin(options[names[i]].rebin);
-	      if(grouped.find(s[j].name)!=grouped.end()) {
+
+	      if(grouped.find(s[j].name) == grouped.end()) {
+		if(debug_)
+		  std::cout << "Cloning and update legend " << std::endl;  
 		grouped[s[j].name]=(TH1F *)h->Clone(("_"+names[i]).c_str());
+		//		std::cout << "Sample : " << s[j].name << " - Integral for plot " << names[i] << " = " << h->Integral(h->GetXaxis()->GetFirst(),h->GetXaxis()->GetLast()) << std::endl;
+		std::cout << "Sample : " << s[j].name << " - Integral for plot " << names[i] << " = " << h->Integral(-10000,10000) << std::endl;
 		l->AddEntry(h,s[j].name.c_str(),"F");
+		mcIntegral += h->Integral();
+		sta->Add(h);
+		hmc->Add(h);
 	      }
-	      else
-		{
-		  grouped[s[j].name]=(TH1F *)h->Clone(("_"+names[i]).c_str());
-		}
-	      sta->Add(h);
-	      //     h->Draw("same");
+// 	      else
+// 		{
+// 		  grouped[s[j].name]=(TH1F *)h->Clone(("_"+names[i]).c_str());
+//	      l->AddEntry(h,s[j].name.c_str(),"F");
+//	      grouped[s[j].name]->Add(h);
+// 		}
 	    }
+	  //h->Draw("same");
 	}
+
+      if(debug_){
+	std::cout << "Data total = " << hd->Integral() << std::endl;
+	std::cout << "MC = " << mcIntegral << std::endl;
+	std::cout << "Data/MC = " << hd->Integral()/mcIntegral << std::endl;
+      }
+
+      TPad * TopPad = new TPad("TopPad","Top Pad",0.01,0.3,0.99,1.00 ) ;
+      TPad * BtmPad = new TPad("BtmPad","Bottom Pad",0.01,0.01,0.99,0.25 ) ;
+      TopPad->SetBottomMargin(0.05) ;
+      BtmPad->SetTopMargin(0.05) ;
+      BtmPad->SetBottomMargin(0.15);
+      TopPad->Draw() ;
+      BtmPad->Draw() ;
+      std::cout << "hd maximum = " << hd->GetMaximum() << "  sta maximum = " << sta->GetMaximum() << std::endl;
+      double maxY;
+      if(hd->GetMaximum() > sta->GetMaximum()) maxY = (hd->GetMaximum())*1.3;
+      else maxY = (sta->GetMaximum())*1.3;
+      TopPad->cd();
+      hd->Draw("E1");
       sta->Draw("same");
       hd->Draw("E1same");
-      hd->GetYaxis()->SetRangeUser(options[names[i]].min,options[names[i]].max);
-      l->Draw();
+      l->Draw("same");
+      std::cout << "Set Maximum to = " << maxY << std::endl;
+      hd->GetYaxis()->SetRangeUser(0.,maxY);
+      hd->GetXaxis()->SetRangeUser(options[names[i]].min,options[names[i]].max);
+
+      BtmPad->cd();
+      std::cout << "Division" << std::endl;
+      TH1D * division = (TH1D*)(hd)->Clone("division");
+      //TH1D * division = new TH1D();
+      division->Sumw2();
+      division->Divide(hmc);
+      division->SetMaximum(2.);
+      division->SetMinimum(0);
+      division->SetMarkerStyle(20);
+      division->GetXaxis()->SetLabelSize(0.06);
+      division->GetXaxis()->SetTitleSize(0.06);
+      division->GetYaxis()->SetLabelSize(0.06);
+      division->GetYaxis()->SetTitle("Data/MC");
+      Double_t min = division->GetXaxis()->GetXmin();
+      Double_t max = division->GetXaxis()->GetXmax();
+      division->Draw("E1");
+
+      TLine *line = new TLine(min, 1.0, max, 1.0);
+      line->SetLineColor(kRed);
+      line->Draw("same");
+
+      c->Update();
+      std::string cName= hd->GetName();
+      cName += "_bare.pdf";
+      cName = path+cName;
+      c->Print(cName.c_str(),"pdf");
+
+
 
       std::cout << names[i] << " d: " <<  hd->Integral() << " ";
       THStack * sta2 = new THStack("sta2",hd->GetTitle());
@@ -129,7 +233,7 @@ void plottingmacro()
 
       if(debug_)
 	std::cout << "Putting the iterator in the for loop" << std::endl;
-      for(std::map<std::string,TH1F *>::iterator it=grouped.begin(); it!=grouped.end();++it)
+      for(std::map<std::string,TH1F *>::reverse_iterator it=grouped.rbegin(); it!=grouped.rend();++it)
  	{
 	  if(debug_)
 	    std::cout << "Using the iterator" << std::endl;
@@ -140,23 +244,27 @@ void plottingmacro()
 	  }
 	  tot+=(*it).second->Integral();
 	  sta2->Add(it->second);
- 	}   
+ 	}
       std::cout << " Tot: " << tot << "+-" << sqrt(toterr2) <<  " SF: " << hd->Integral()/tot << std::endl;
-      c = new TCanvas();
-      if (hd->GetMaximum() > sta2->GetMaximum()) sta2->SetMaximum(hd->GetMaximum());
-      else sta2->SetMaximum(sta2->GetMaximum());
-      sta2->Draw();
-      hd->Draw("E1,same");
-      sta2->GetYaxis()->SetRangeUser(options[names[i]].min,options[names[i]].max);
-      hd->GetYaxis()->SetRangeUser(options[names[i]].min,options[names[i]].max);
-      l->Draw();
+      TCanvas *c2 = new TCanvas();
+      c2->SetTitle(names[i].c_str());
+      std::cout << "hd maximum = " << hd->GetMaximum() << "  sta2 maximum = " << sta2->GetMaximum() << std::endl;
+      if(hd->GetMaximum() > sta2->GetMaximum()) maxY =  hd->GetBinContent(hd->GetMaximumBin()) * 1.3;
+      else maxY = ( sta2->GetMaximum())*1.3;
+      //      hd->Draw("E1"); 
+      sta2->Draw("PADS");
+      //    hd->Draw("E1same");
+      //      l->Draw("same");
+      std::cout << "Set Maximum to = " << maxY << std::endl;
+      hd->GetYaxis()->SetRangeUser(0.,maxY);
+      hd->GetXaxis()->SetRangeUser(options[names[i]].min,options[names[i]].max);
+      c2->Update();
 
 
-      
-
-      c->Print("Test.pdf");
-   
-   
+      std::string c2Name = hd->GetName();
+      c2Name = path+c2Name;
+      c2Name += "_norm.pdf";
+      c2->Print(c2Name.c_str(),"pdf");  
 
     }
 
