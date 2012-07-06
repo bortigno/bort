491344294d81e2ba61b0ddad76dfb167529c2090
bortigno <>
2012-07-06 08:50:25 +0000

    dummy script to run addingSamples.py

---
+++ Utils/prepareDY.sh
@@ -0,0 +1,5 @@
+./addingSamples.py
+hadd noHistos.DYJets.root lheWeight*
+./addHistos.py -i ZllH.Jun15.DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball.root -t noHistos.DYJets.root
+#rm -rf lheWeight*
+#rm -rf noHisto* 
