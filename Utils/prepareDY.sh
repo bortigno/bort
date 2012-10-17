327ce16c4382fe5e1eb52965a3bf319a5c337ea8
bortigno <>
2012-10-17 13:07:13 +0000

    hadd not needed

--- Utils/prepareDY.sh
+++ Utils/prepareDY.sh
@@ -1,5 +1,5 @@
 ./addingSamples.py
-hadd noHistos.DYJets.root lheWeight*
+#hadd noHistos.DYJets.root lheWeight*
 ./addHistos.py -i ZllH.Jun15.DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball.root -t noHistos.DYJets.root
 #rm -rf lheWeight*
 #rm -rf noHisto* 
